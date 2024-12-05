029b72ccbd7c4b80c5686cb1c49352a9c3e55e96
shaojinyu <1009943858@qq.com>
2024-12-05 18:46:46 +0800

    fix

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 import * as path from "path";
+import { fileURLToPath } from "url";
 
 export type Config = {
   input?: string;
@@ -6,8 +7,11 @@ export type Config = {
   ignoreFields?: string[];
 };
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const defaultConfig: Config = {
   input: path.join(process.cwd(), "./prisma/schema.prisma"),
-  output: path.resolve("./generate/schema.js"),
+  output: path.join(__dirname, "../generate/schema.js"),
   ignoreFields: ["createdAt", "updatedAt"],
 };
